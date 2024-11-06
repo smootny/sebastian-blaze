4e5408b21324a2520f41056fb194b2e1d8e1ddb1
smootny <blazejewski@post.pl>
2024-11-07 00:33:19 +0100

    added skills

--- sebastian-blaze/src/app/app.component.ts
+++ sebastian-blaze/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { ProjectsComponent } from "./projects/projects.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,9 @@ import { ProjectsComponent } from "./projects/projects.component";
   imports: [
     RouterOutlet,
     HeroComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    SkillsComponent,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
