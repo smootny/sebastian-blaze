780299b0906976ec247c4f3c6d9b2c954e1a0536
smootny <blazejewski@post.pl>
2024-11-10 16:55:57 +0100

    added rensovness for mobiles

--- sebastian-blaze/src/app/projects/projects.component.ts
+++ sebastian-blaze/src/app/projects/projects.component.ts
@@ -1,17 +1,36 @@
+
+//   openLink(url: string): void {
+//     window.open(url, '_blank'); // Opens the link in a new tab
+// }
+
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrls: ['./projects.component.css'],
+  imports: [NgFor]
 })
 export class ProjectsComponent {
+  activeProjectIndex = 0; 
+  projectCount = 3; 
 
+  setActiveProject(index: number) {
+    this.activeProjectIndex = index;
+  }
 
-  openLink(url: string): void {
-    window.open(url, '_blank'); // Opens the link in a new tab
-}
+  nextProject() {
+    this.activeProjectIndex = (this.activeProjectIndex + 1) % this.projectCount;
+  }
 
+  previousProject() {
+    this.activeProjectIndex =
+      (this.activeProjectIndex - 1 + this.projectCount) % this.projectCount;
+  }
+
+  isActive(index: number): boolean {
+    return this.activeProjectIndex === index;
+  }
 }
